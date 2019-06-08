8e235e5e2383402ca0327e40e865ea2610a49850
Doyeong Lee <woozu99@naver.com>
2019-06-08 14:33:11 +0900

    mypage에서 사용자 정보 변경시 구매자, 판매자 별 변경 페이지 구현 및  db 데이터   변경

--- check_right_pwd.php
+++ check_right_pwd.php
@@ -2,8 +2,21 @@
 
     include './dbcon.php';
 
+    $login_id=$_POST['login_id'];
+
     if($_POST['login_pwd']==$_POST['PW_old']){
-      echo "<form name='right_pwd' action='./change_user_info.html' method='post'>";
+      if($_POST['login_check_info']=='buyer'){
+        echo "<form name='right_pwd' action='./change_buyer_info.html' method='post'>";
+        $query="SELECT name from buyer where ID='$login_id'";
+      } else{
+        echo "<form name='right_pwd' action='./change_seller_info.html' method='post'>";
+        $query="SELECT name from seller where ID='$login_id'";
+      }
+
+      $result = mysqli_query($connect, $query);
+      $row = mysqli_fetch_array($result);
+
+      echo "<input type='hidden' name='name' value='{$row['name']}'>";
       echo "<input type='hidden' name='login_id' value='{$_POST['login_id']}'>";
       echo "<input type='hidden' name='login_pwd' value='{$_POST['login_pwd']}'>";
       echo "<input type='hidden' name='login_check_info' value='{$_POST['login_check_info']}'></form>";
