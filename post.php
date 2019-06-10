4a189ee96a9e79415d96c46c14746a13b1f72809
yujin lee <myujin3927@gmail.com>
2019-06-10 16:45:33 +0900

    이유진 0610작업내용

--- post.php
+++ post.php
@@ -5,12 +5,19 @@
     $title = $_POST['b_title'];
     $contents = $_POST['b_content'];
     $uid = $_POST['b_writer'];
+    $id=$_POST['login_id'];
+    $pwd=$_POST['login_pwd'];
+    $check_info=$_POST['login_check_info'];//구매자인지 판매자인지 확인하는 변수
     $contents = nl2br($contents);
 
-    $query="INSERT into board(title, content, writer, count) values('$title', '$contents', '$uid', 0)";
+    $query="INSERT into purchase(title, content, writer, count) values('$title', '$contents', '$uid', 0)";
 
     mysqli_query($connect, $query);
 
-    echo "<script>alert('게시글이 작성되었습니다.'); location.href='./main.php';</script>";
+    echo "<form name='logged' action='./item.php' method='post'>";
+    echo "<input type='hidden' name='login_id' value='$id'>";
+    echo "<input type='hidden' name='login_pwd' value='$pwd'>";
+    echo "<input type='hidden' name='login_check_info' value='$check_info'></form>>";
 
+    echo "<script>alert('게시글이 작성되었습니다.');document.logged.submit();</script>";
  ?>
--- regist_item.php
+++ regist_item.php
@@ -1,11 +1,8 @@
-<?
-/***item.php***/
-/***역할: (추가) ***/
-?>
+
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <html>
 <head>
-<title>판매 상품 목록</title>
+<title>상품 등록</title>
 <link rel="stylesheet" href="css/home.css">
 <head>
 		<meta charset="utf-8">
@@ -13,11 +10,6 @@
 </head>
 <body>
 <div id="page-wrapper">
-
-	<?
-	/*홈페이지, 아이템페이지, 마이페이지 이동 버튼 구현 및 다음 페이지로 정보를 전달*/
-	/*이동 정보: 로그인 된 아이디, 비밀번호, 구매자인지 판매자인지 확인하는 변수(login_check_info)*/
-	?>
 	<div id="head">
 		<nav>
 			<form class="head" action="./home.php" name='home' method="post">
@@ -41,12 +33,12 @@
 		</nav>
 	</div>
 
-	<h1>판매 상품 목록</h1>
+	<h1>상품 등록</h1>
 	<div id="content">
 		<section id="main-section">
 
 
-    <h2>이번 주 랭킹</h2>
+    <h2>상품 등록</h2>
 		이번 주에 가장 많이 판매된 상품입니다.
 
 		<h2>내 상품 등록하기</h2>
@@ -64,7 +56,7 @@
 			<tr>
 				<td><label>글쓴이</label></td>
 			<?
-				if ( $_POST['login_check_info'] ) {
+				if ( $_POST['login_success'] ) {
 			?>
 					<td><?=$_POST['login_id']?><input type="hidden" name="b_writer" value="<?=$_POST['login_id']?>"></td>
 				</tr>
@@ -75,40 +67,9 @@
 			<?
 				}
 			?>
-			<input type='hidden' name='login_id' value=<?=$_POST['login_id']?>>
-			<input type='hidden' name='login_pwd' value=<?=$_POST['login_pwd']?>>
-			<input type='hidden' name='login_check_info' value=<?=$_POST['login_check_info']?>>
-		<tr><td colspan="2" align="center"><input type="submit" value="게시글 쓰기"/> &nbsp;</td></tr>
+		<tr><td colspan="2" align="center"><input type="submit" value="게시글 쓰기"/> &nbsp; <a href="./login_form.html">로그인</a></td></tr>
 		</table>
 		</form>
-
-		<br><br>
-		<h2> 게시판 </h2>
-		<table width= "800" border="1" cellspacing="0" cellpadding="5">
-		<tr align="center">
-			<td bgcolor="#cccccc">번호</td>
-			<td bgcolor="#cccccc">제목</td>
-			<td bgcolor="#cccccc">글쓴이</td>
-			<td bgcolor="#cccccc">조회수</td>
-		</tr>
-	<?
-
-		include './dbcon.php';
-
-		$query = "SELECT * from board order by id desc limit 5";
-
-		$result = mysqli_query($connect, $query);
-
-		while ($row = mysqli_fetch_array($result)) {
-	?>
-				<td><a href="content.php?id=<?=$row['id']?>"><?=$row['id']?></a></td>
-				<td><?=$row['title']?></td>
-				<td><?=$row['writer']?></td>
-				<td><?=$row['count']?></td>
-			</tr>
-	<?
-		}
-	?>
 			</section>
 
 			<aside id="main-aside">
